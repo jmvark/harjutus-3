708c2cae7529d3cd1a5c5ffc2f6b3465852a1d38
JasonMax_2 <jason-max.vark@khk.ee>
2014-09-16 14:44:20 +0300

    Add sample variables2

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;
+charset=utf-8">
+<title>harjutus-3</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<h2>Muutujad</h2>
+<?php
+    $variable = 10;
+    echo $variable;
+    // Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
+    echo "<br>";
+?>
+<?php
+    $variable = 20;
+    echo $variable;
+?>
+
+// Selles näites on "Hello world!" string, mis koosneb 12 tähemärgist (ka tühik ning hüüumärk on tähemärgid).
+<?php echo "Hello world!"; ?>
+
+<h2>Stringid</h2>
+
+
+</body>
+</html>
