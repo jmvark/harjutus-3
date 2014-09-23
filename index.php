99df923527afedb73bce82ae949a7bf7d6936b72
JasonMax_2 <jason-max.vark@khk.ee>
2014-09-23 09:31:33 +0300

    Add sample content echo function

--- index.php
+++ index.php
@@ -18,6 +18,51 @@ charset=utf-8">
     $variable = 20;
     echo $variable;	
 	?>
+<h2>Stringid</h2>
+<?php echo "Hello world!"; 
+echo "<br>"; 
+?>
+<?php echo 'Tere maailm!'; 
+echo "<br>"; 
+?>
+<?php echo "24"; 
+echo "<br>"; 
+?>
+<?php echo "26"; 
+echo "<br>"; 
+?>
+<?php echo "2426"; ?>
+<h2>Nimekirja väljastamine muutujate ja stringide abil</h2>
+<?php 
+	$student1 = 'Jaan' . $fullname ' Värk';
+	$student2 = 'Jaanus';
+	$student3 = 'Joonas';
+	$student4 = 'Mari';
+	$student5 = 'Maria';
 
+?>
+<ul>
+	<li>
+	<?php echo $student1; 
+	?>
+	</li>
+<li>
+	<?php echo $student2; 
+	?>
+	</li>
+<li>
+	<?php echo $student3; 
+	?>
+	</li>
+<li>
+	<?php echo $student4; 
+	?>
+	</li>
+<li>
+	<?php echo $student5; 
+	?>
+	</li>
+
+</ul>
 </body>
 </html>
